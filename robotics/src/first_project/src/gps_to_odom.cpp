a37fd487b18a65a17f300821095dd68868175ab8
giammu <samuele.giammusso@mail.polimi.it>
2024-04-16 10:19:11 +0200

    first node and launch file

--- robotics/src/first_project/src/gps_to_odom.cpp
+++ robotics/src/first_project/src/gps_to_odom.cpp
@@ -18,20 +18,33 @@ Il gps ti dà una posizione ma non l'orientation, devo computarla: ho multiple p
 #include "ros/ros.h" 
 #include "nav_msgs/Odometry.h" 
 
-void function(const nav_msgs::Odometry::ConstPtr& msg){ 
+void callback(const nav_msgs::Odometry::ConstPtr& msg){ //funzione chiamata automaticamente ogni volta che arriva un nuovo messaggio
     ROS_INFO("I heard: [%s]", msg->data.c_str()); //processing dei data: sbagliato il tipo di dato
+
+    //qui computo i dati e poi li pubblico???
+
 }
+
+
 
 int main(int argc, char **argv){
 
     ros::init(argc, argv, "gps_to_odom");
     ros::NodeHandle n; //forse devo usare più di 1 handler per fare pub e sub in contemporanea
 
-    ros::Subscriber sub = n.subscribe("fix", 1, function); //topic: fix, buffer: dimensione 1, il subscriber chiama la funzione
+    //Subscriber
+    ros::Subscriber sub = n.subscribe("fix", 1, callback); //topic: fix, buffer: dimensione 1, il subscriber chiama la funzione
+    ros::spin(); //cicla aspettando i messaggi
 
+
+
+    //Publisher
    	ros::Publisher pub = n.advertise<nav_msgs::Odometry>("gps_odom", 1); // type: nav_msgs/Odometry, topic: gps_odom, buffer: dimensione 1 (good practice)
+    nav_msgs::Odometry odom; //creo il messaggio
+    odom.pose.x = 1; //assegnao il valore (probabilmente sbagliato)
+    pub.publish(odom);
 
-
+    //Loop
     ros::Rate loop_rate(10); //loop MANUALE a 10hz 
   	while (ros::ok()){ //loop
         //qua eseguo qualcosa
