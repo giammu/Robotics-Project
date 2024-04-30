b30bf8d6e619d0b5ba9f3800685fb02df4c96696
giammu <samuele.giammusso@mail.polimi.it>
2024-04-30 23:12:16 +0200

    Modified lidar_remap

--- robotics/src/first_project/src/lidar_remap.cpp
+++ robotics/src/first_project/src/lidar_remap.cpp
@@ -11,7 +11,6 @@ Il nodo deve permettere all'utente di selezionare da rqt_reconfigure a quale tf 
 
 */
 
-
 #include "sensor_msgs/PointCloud2.h"
 #include "string.h"
 #include <sstream>
@@ -21,40 +20,41 @@ Il nodo deve permettere all'utente di selezionare da rqt_reconfigure a quale tf 
 #include <first_project/parametersConfig.h> // nome_package/"stringWroteInsideTheConfigurationFile" combined with "Config.h"
 
 
+sensor_msgs::PointCloud2 message;
+
 void callback2(const sensor_msgs::PointCloud2& msg){ 
 
-  //msg.header.frame_id = config.header;
+    message = msg;  
 
-    ROS_INFO("header %s", msg.header.frame_id.c_str());  //print if the reference is gps or wheel odom
+    ROS_INFO("vecchio header %s", msg.header.frame_id.c_str());  //print if the reference is gps or wheel odom
     
 }
 
-
 void callback(first_project::parametersConfig &config, uint32_t level) { //la callback function ha 2 parametri: config=la lista di tutti i values, level= cioè quale parametro cambio (quello che prima abbiamo definito con 0,1,2,3,4...). uso uint32_t perchè solo 1 parmetro può cambiare alla volta, e quindi è sufficiente
-    ros::NodeHandle n; 
     
-    ROS_INFO("Reconfigure Request: %s ", config.header.c_str());  
-    ROS_INFO ("%d",level); //se voglio vedere quale sta cambiando mi basta vedere il level
+    ros::NodeHandle n; 
+
+    ROS_INFO("Reconfigure Request: %s, level %d ", config.header.c_str(), level);  //se voglio vedere quale sta cambiando mi basta vedere il level
 
     ros::Subscriber sub = n.subscribe("os_cloud_node/points", 1, callback2); 
+
+    message.header.frame_id = config.header;
+
+    ros:: Publisher pub = n.advertise<sensor_msgs::PointCloud2>("pointcloud_remapped", 1); // type: nav_msgs/Odometry, topic: gps_odom, buffer: dimensione 1 (good practice)
     
-    ros:: Publisher pub = n.advertise<sensor_msgs::PointCloud2>("/pointcloud_remapped", 1); // type: nav_msgs/Odometry, topic: gps_odom, buffer: dimensione 1 (good practice)
-    
+    pub.publish(message);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lidar_remap"); //inizializzo il ros node
-
+    
     dynamic_reconfigure::Server<first_project::parametersConfig> server; //prima creo il sever: il tipo del server deve essere parameter_test::parametersConfig, cioè nome del package::nome del config file
-    dynamic_reconfigure::Server<first_project::parametersConfig>::CallbackType f; //qua creo la callback
+    dynamic_reconfigure::Server<first_project::parametersConfig>::CallbackType f; //qua creo la callback 
 
-    //Subscriber
-    //ros::Subscriber sub = n.subscribe("os_cloud_node/points", 1); 
-
-    f = boost::bind(&callback, _1, _2); //then i use boost to bind the callback che ho appena definito (riga 20) con la funzione che si occupa di fare la callback (riga 6)
+    f = boost::bind(&callback, _1, _2); //then i use boost to bind the callback che ho appena definito con la funzione che si occupa di fare la callback 
     server.setCallback(f); //gli passo la callback che ho appena creato
 
-    ROS_INFO("Spinning node");
     ros::spin(); //il nodo continua ad andare in attesa che succeda qualcosa sulla dynamic reconfigure, triggerando la callback
     return 0;
 }
+
