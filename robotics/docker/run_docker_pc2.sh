989381121c8811e5dd4044e4b88724912ada108c
giammu <samuele.giammusso@mail.polimi.it>
2024-05-15 13:44:18 +0200

    L7 files + slides + new docker

---
+++ robotics/docker/run_docker_pc2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+CONTAINER_NAME="robotics_second_pc"
+
+# Check if the container exists
+if [ $(docker ps -a -q -f name=^/${CONTAINER_NAME}$) ]; then
+    echo "Container '${CONTAINER_NAME}' already running. Removing it ..."
+    docker rm -f $CONTAINER_NAME
+    echo "Container '${CONTAINER_NAME}' has been removed."
+else
+    echo "Container '${CONTAINER_NAME}' does not exist."
+fi
+
+echo "Starting '${CONTAINER_NAME}' container."
+
+MOUNT_PATH="/home/$USER/catkin_ws/src"
+SRC_PATH="$(pwd)/../src"
+
+# Start the container. 
+docker run -it \
+    --net=ros \
+    --env="DISPLAY=novnc:0.0" \
+    --volume="${SRC_PATH}:${MOUNT_PATH}" \
+     --name=$CONTAINER_NAME \
+    robotics:stable \
+    /bin/bash 
