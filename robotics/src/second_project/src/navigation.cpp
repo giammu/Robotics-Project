92330e9950df23fd013e044833f0a92b6647fd8a
giammu <samuele.giammusso@mail.polimi.it>
2024-06-23 23:14:05 +0200

    started navigation (not working)

---
+++ robotics/src/second_project/src/navigation.cpp
@@ -0,0 +1,13 @@
+#include <ros/ros.h>
+#include <fstream>
+#include <sstream>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/transform_broadcaster.h>
+
+typedef actionlib::SimpleActionClient <move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+
+int main(int argc, char **argv) {<
+    return 0;
+}
